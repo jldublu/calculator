68ebcc5162cac67afb8400698e7aa6f9252b12d5
Jonathan Walker <jonwalker302@gmail.com>
2021-09-02 22:06:39 -0400

    Added ability for calculator to evaluate math expression from display

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
+let number1;
+let number2;
+let operator;
+let displayElement = document.querySelector('.display');
+
 window.onclick = function(e) {
   let element = e.target;
   console.log(element.className);
   switch (element.className) {
     case 'number':
     case 'operator':
-      updateUserInput(element.outerText);
+      updateDisplay(element.outerText);
       break;
     case 'evaluate':
+      parseValuesFromDisplay(displayElement.value);
+      let result = operate(operator, number1, number2);
+      clearDisplay();
+      resetValues();
+      updateDisplay(result);
+      number1 = result;
+      result = undefined;
       break;
     case 'clear':
+      clearDisplay();
+      resetValues();
       break;
     case 'backspace':
       break;        
     default: 
       console.log(`No action can be done on ${element}`);
   }
+
 };
 
 function add(number1, number2) {
-  return number1 + number2;
+  return Number(number1) + Number(number2);
 }
 
 function subtract(number1, number2) {
@@ -50,11 +65,55 @@ function operate(operator, number1, number2) {
     default:
       console.log('Operator not recognized...');  
   }
+
 }
 
-function updateUserInput(value) {
-  let element = document.querySelector('.user-input');
-  let currentInput = element.value;
-  document.querySelector('.user-input').value = currentInput + value;
-  let attributes = document.querySelector('.user-input').attributes;
+function clearDisplay() {
+  displayElement.value = '';
+}
+
+function parseValuesFromDisplay(displayText) {
+  let splitValue;
+  if (displayText.includes('+')) {
+    splitValue = '+';
+  } else if (displayText.includes('-')) {
+    splitValue = '-';
+  } else if (displayText.includes('*')) {
+    splitValue = '*';
+  } else if (displayText.includes('/')) {
+    splitValue = '/';
+  } 
+
+  if (splitValue) {
+    operator = splitValue;
+    let displayArray = displayText.split(splitValue);
+    if (isNaN(displayArray[0])) {
+      clearDisplay();
+      updateDisplay('Error');
+    } else {
+      number1 = displayArray[0];
+    }
+
+    if (isNaN(displayArray[1])) {
+      clearDisplay();
+      updateDisplay('Error');
+    } else {
+      number2 = displayArray[1];
+    }
+
+  } else {
+    clearDisplay();
+    updateDisplay('Error');
+  }
+  
+}
+
+function resetValues() {
+  number1 = undefined;
+  number2 = undefined;
+  operator = undefined;
+}
+
+function updateDisplay(value) {
+  displayElement.value = displayElement.value + value;
 }
